a5dc88c0d809dc9fd3c7070c4b478a599a99aa4d
njoum.abdullah@gmail.com <njoum.abdullah@gmail.com>
2024-10-15 12:15:51 +0200

    Cart Details Page Implementation

--- src/app/components/app-content/cart-details/cart-details.component.ts
+++ src/app/components/app-content/cart-details/cart-details.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from '../../../shared/services/shopping-cart.service';
+import { Product } from '../../../shared/models/product.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cart-details',
   templateUrl: './cart-details.component.html',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   styleUrls: ['./cart-details.component.scss']
 })
 export class CartDetailsComponent implements OnInit {
-
-  constructor() { }
-
+  cartItems: Product[] = [];
   ngOnInit() {
+    this.cartItems = this.cartService.getCartItems();
   }
-
+  
+  constructor(private readonly cartService: ShoppingCartService) {}
 }
